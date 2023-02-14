835762a5bf54052c8d650794ead7d723de84e8ad
kimsang <boh001g@gmail.com>
2023-02-14 22:07:41 +0900

    [MOD] Modify scripts

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PROJECT_PATH="$(pwd)"
+
 function has_node {
   local NODE_NAME=$1
   multipass ls | grep "$NODE_NAME"
@@ -11,6 +13,6 @@ function start_node {
   if [[ $(has_node "$NODE_NAME") ]]; then
     multipass start "$NODE_NAME"
   else
-    multipass launch -c 2 -m 2G -d 20G -n "$NODE_NAME" 20.04
+    multipass launch -c 2 -m 2G -d 10G -n "$NODE_NAME" --mount $PROJECT_PATH:~/common 22.04
   fi
 }
